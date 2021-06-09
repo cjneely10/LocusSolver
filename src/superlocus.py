4e837ca38fc25703d76507c9b4e2666984b1162e
Christopher Neely <christopher.neely1200@gmail.com>
2021-06-09 11:37:17 -0700

    Begin merging algorithm

--- src/superlocus.py
+++ src/superlocus.py
@@ -3,15 +3,27 @@ from typing import Dict, List
 
 class Feature:
     def __init__(self, start: int, end: int, strand: int):
-        self.start = start
-        self.end = end
-        self.strand = strand
+        self._start = start
+        self._end = end
+        self._strand = strand
+
+    @property
+    def start(self):
+        return self._start
+
+    @property
+    def end(self):
+        return self._end
+
+    @property
+    def strand(self):
+        return self._strand
 
 
 class SuperLocus:
-    def __init__(self, start: int, end: int):
-        self._start = start
-        self._end = end
+    def __init__(self):
+        self._start = -1
+        self._end = -1
         # Store features by strand
         self._features: Dict[int, Dict[str, List[Feature]]] = {1: {}, -1: {}}
 
@@ -29,3 +41,7 @@ class SuperLocus:
             strand_dict[identifier] = []
         strand_dict[identifier].append(feature)
         strand_dict[identifier].sort(key=lambda feat: feat.start)
+        if feature.start < self._start:
+            self._start = feature.start
+        if feature.end > self._end:
+            self._end = feature.end
--- src/superlocus_tree.py
+++ src/superlocus_tree.py
@@ -1,3 +1,18 @@
+from src.superlocus import SuperLocus, Feature
+
+
 class SuperLocusTree:
+    class _TreeNode:
+        def __init__(self, super_locus: SuperLocus):
+            self.current = super_locus
+            self.left = None
+            self.right = None
+
     def __init__(self):
+        self.head = None
+
+    def insert(self, feature: Feature):
+        pass
+
+    def sort(self):
         pass
