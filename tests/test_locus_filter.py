a2968d53134099acea15c0c5b862e9dd6a6a7c23
Christopher Neely <christopher.neely1200@gmail.com>
2021-06-10 10:35:57 -0700

    Add test data

--- tests/test_locus_filter.py
+++ tests/test_locus_filter.py
@@ -9,13 +9,13 @@ from src.locus_filter import LocusFilter
 class TestTransitionModel(unittest.TestCase):
     def test_load(self):
         model1 = Annotation(
-            Path("data/GCA_000151265.1_Micromonas_pusilla_CCMP1545_v2.0_genomic.mask.Repeats.fna"),
-            Path("data/GCA_000151265.1_Micromonas_pusilla_CCMP1545_v2.0_genomic.AbinitioGeneMark.gff3"),
+            Path("data/sample.fna"),
+            Path("data/sample.gmes.gff3"),
             "genemark"
         )
         model2 = Annotation(
-            Path("data/GCA_000151265.1_Micromonas_pusilla_CCMP1545_v2.0_genomic.mask.Repeats.fna"),
-            Path("data/GCA_000151265.1_Micromonas_pusilla_CCMP1545_v2.0_genomic.augustus.gff3"),
+            Path("data/sample.fna"),
+            Path("data/sample.aug.gff3"),
             "augustus"
         )
         locus_filter = LocusFilter(annotation_models=[model1, model2])
