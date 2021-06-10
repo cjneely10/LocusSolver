fcc275042d06f8d770b8ea40f25b793b42fea766
Christopher Neely <christopher.neely1200@gmail.com>
2021-06-09 21:47:23 -0700

    Begin implementing filter function using any callable object -> conver SuperLocus to single Feature, which is then parsed to a SeqRecord for writing to file

--- src/models/locus_filter.py
+++ src/models/locus_filter.py
@@ -1,4 +1,6 @@
-from typing import List, Dict
+from typing import List, Dict, Callable, Generator
+
+from Bio.SeqRecord import SeqRecord
 
 from src.models.annotation_model import AnnotationModel
 from src.util.superlocus import SuperLocus
@@ -8,10 +10,10 @@ class LocusFilter:
     def __init__(self, annotation_models: List[AnnotationModel]):
         self._models = annotation_models
         self.feature_slls: Dict[str, List[SuperLocus]] = AnnotationModel.merge(annotation_models)
-        for contig_id, sll in self.feature_slls.items():
-            print(contig_id)
-            for super_locus in sll:
-                print(super_locus)
+        # for contig_id, sll in self.feature_slls.items():
+        #     print(contig_id)
+        #     for super_locus in sll:
+        #         print(super_locus)
 
-    def filter(self):
+    def filter(self, fxn: Callable[[SuperLocus], SeqRecord]) -> Generator[SeqRecord, None, None]:
         pass
