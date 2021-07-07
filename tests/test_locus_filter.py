1a268c7b7f285a3c93212cda064907f1d9e78a32
Christopher Neely <christopher.neely1200@gmail.com>
2021-07-06 20:19:46 -0700

    Add data

--- tests/test_locus_filter.py
+++ tests/test_locus_filter.py
@@ -11,15 +11,15 @@ from src.util.annotation import Annotation
 class TestLocusFilter(unittest.TestCase):
     def test_load(self):
         model1 = Annotation(
-            Path("data/sample.gmes.gff3"),
+            Path("../data/sample.gmes.gff3"),
             "1"
         )
         model2 = Annotation(
-            Path("data/sample.aug.gff3"),
+            Path("../data/sample.aug.gff3"),
             "2"
         )
         locus_filter = LocusFilter(annotation_models=[model1, model2])
-        with open("data/sample.merged.gff3", "w") as merged_file:
+        with open("../data/sample.merged.gff3", "w") as merged_file:
             GFF.write(locus_filter.filter(Tier(1, {1: "1", 2: "2"})), merged_file)
 
 
